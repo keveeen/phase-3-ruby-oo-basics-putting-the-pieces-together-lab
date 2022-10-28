cb88d2c2d7c29e56f7b1cdf8e448fb5960da7740
keveeen <105822487+keveeen@users.noreply.github.com>
2022-10-28 23:19:17 +0200

    commit tree 940fd3103dfe8da744ec52d351b9f11c5f105b1c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
 
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+
+    def initialize(title = "And Then There Were None")
+        # @book = book
+        @title = title
+    end
+
+    def turn_page
+
+        puts "Flipping the page...wow, you read fast!"
+
+    end
+
+
+end
 
